313e61568a863cee5d283099f50fe38b808f4635
JunayedFahad <154872830+JunayedFahad@users.noreply.github.com>
2025-07-23 13:41:06 +0600

    Add files via upload

---
+++ lab_task2.0.php
@@ -0,0 +1,66 @@
+<!Doctype html>
+<html>
+    <body>
+        <table>
+        <title>Fahad</title>
+       <center>
+    <h1 style = "color:blue;">Bank Management System </h1>
+
+    <h4 aling "Right">Your Trusted Finalcial Partner </h4>
+</center>
+      <h4 align = "Left">Customer Registration Form</h4>
+    <tr>
+          
+      <td> Full Name : </td>
+     
+      <input type ="text"> <br> <br> 
+
+</tr>
+<tr>
+      <td> Date of Birth :</td>
+        
+      <td><input type ="Date"> </td>
+</tr>
+
+ <tr>
+
+    <td>  Gender :</td>
+      
+        <td>
+      <input type = "radio" name ="des"> Male 
+      <input type = "radio" name ="des"> Female 
+      <input type = "radio" name ="des"> Others    
+</td>
+</tr>
+
+<tr>
+     <td> Select..</td>
+     <td>
+</tr>
+     Account Type :
+<tr>
+    <td>
+     <select>
+        <option>Savings</option>
+         <option>Deposit</option>
+     </select> <br> <br>
+</td>
+       
+</tr>
+       <tr>
+        <td> Initial Deposit Amount : </td>
+   
+         
+      <td> <input type ="text"> </td> 
+
+       
+       </tr>
+       <tr>
+        <td>
+            Mobile Number:
+        </td>
+</tr>
+
+</table>
+    </body>
+</html>
