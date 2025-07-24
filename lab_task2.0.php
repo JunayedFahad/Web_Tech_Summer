13cabb73e26c7b3be76a1d4e395e11f504684657
junayed fahad <junayedfahad22@gmail.com>
2025-07-24 22:02:25 +0600

    initial commit

---
+++ lab_task2.0.php
@@ -0,0 +1,148 @@
+<!Doctype html>
+<html>
+
+<body>
+    <title>WEB TECH</title>
+
+<center>
+    <h1 style = "color :blue">Bank Management System</h1>
+    <h3 style = "color :Red">Your Trusted Finalcial Partner</h3>
+
+</center>
+  <h4 align = "Left">Customer Registration Form </h4>
+  <table>
+    <tr>
+        <td>FullName:</td>
+         <td> <input type ="text"> </td>
+
+        </tr>
+<tr>
+        <td>Date of Birth:</td>
+         <td> <input type ="date"> </td>
+
+        </tr>
+<tr>
+        <td> Gender: </td>
+
+         <td> <input type ="radio"name="des">Male</td>
+
+         <td> <input type ="radio"name="des">Female</td>
+
+         <td> <input type ="radio"name="des">Others</td>
+
+        </tr>
+
+
+        <tr>
+
+            <td> Mertial Status</td>
+            <td> <select name ="Status">
+                <option value ="">.select</option>
+                <option value ="">Married</option>
+                <option value ="">Unmarried</option>
+                <option value ="">Divorce</option>
+</td>
+                
+</tr>
+
+<tr>
+
+            <td>Inital Deposit Amount</td>
+            <td><input type ="text">
+                
+</td>
+                
+</tr>
+
+
+<tr>
+
+            <td>Mobile Number</td>
+            <td><input type ="text">
+                
+</td>
+                
+</tr>
+
+<tr>
+
+            <td>Occupation</td>
+            <td><input type ="text">
+                
+</td>
+                
+</tr>
+
+
+<tr>
+
+            <td>National</td>
+            <td><input type ="text">
+                
+</td>
+                
+</tr>
+
+<tr>
+
+            <td>Upload ID Proof</td>
+            <td><input type ="file">
+                
+</td>
+                
+</tr>
+
+
+<tr>
+
+            <td>Mobile Number</td>
+            <td><input type ="text">
+                
+</td>
+                
+</tr>
+
+
+    <tr>
+
+            <td><input type = "checkbox">I agree with the terms and condition</td>
+           
+                
+</td>
+                
+</tr>
+<tr>
+<td> <input type = "Submit"> </td>
+<td> <input type = "Reset"></td>
+
+    </table>
+<style>
+
+body{background-color : lightgreen;}
+table{background-color:#fff;
+
+border : 1px solid #ccc
+margin : auto ;
+padding : 20px;
+
+
+
+}
+
+
+
+input[type = "Submit"],input[type ="Reset"]
+
+{
+    padding : 5px ;
+    color : Yellow ;
+    background : skyblue ;
+    cursor : pointer ;
+
+}
+
+</style>
+</body>
+
+
+</html>
